4473e12d3e07e2b9159765f6b5357ba70b439078
MephistoDevelop <inesherald@gmail.com>
2020-03-15 03:23:59 -0600

    Add Skeleton folders and phaser cdn

---
+++ src/index.js
@@ -0,0 +1,6 @@
+(() => {
+  const content = document.getElementById('content');
+  const text = document.createElement('h1');
+  text.textContent = 'Im Webpack Master Pro !!';
+  content.append(text);
+})();
